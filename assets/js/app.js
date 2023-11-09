81eb5b4dc00b43bb9e6e2b3d92f21419f8316b2f
harsh-sheoran399 <harshsheoran9350@gmail.com>
2023-11-08 12:46:55 -1200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+$('.slide').slick({
+    dots: true,
+    infinite: true,
+    speed: 100,   
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '.buttonprev',
+    nextArrow: '.buttonnxt',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true
+        }
+    },
+
+    ]
+});
+
+
